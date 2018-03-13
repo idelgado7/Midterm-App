60268097edddbe3cea20dd43ffb3e4abdeb3556e
Irving Delgado <irving@idelgad7e0eb.nmsu.edu>
2018-03-12 18:46:56 -0600

    Added the third trade view controller. Began working on it.

---
+++ Trading/TradeViewController.swift
@@ -0,0 +1,44 @@
+//
+//  TradeViewController.swift
+//  Midterm App
+//
+//  Created by Irving Delgado on 3/12/18.
+//  Copyright © 2018 Irving Delgado. All rights reserved.
+//
+
+import UIKit
+
+class TradeViewController: UITradeViewController, UITradeViewControllerDelegate, UITradeViewControllerDataSource {
+    
+    func newVc(viewController: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: viewController)
+    }
+    
+    lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newVc(viewController: "sbTab1"),
+                self.newVc(viewController: "sbTab2")]
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
